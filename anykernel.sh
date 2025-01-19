674e91cecd225528460ad754c20be675cc859e8e
JacoaFur <70498193+JacoaFur@users.noreply.github.com>
2025-01-19 09:21:27 -0500

    Update anykernel.sh
    Definitely fixed now

--- anykernel.sh
+++ anykernel.sh
@@ -7,7 +7,7 @@ properties() { '
 kernel.string=LineageOS 22.1 KernelSU-Next Kernel by Jacoa
 do.devicecheck=0
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 '; } # end properties
